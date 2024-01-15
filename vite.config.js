7c3ea4772672902df9f88140f98782f4e37bbe81
cristinanico <135840337+cristinanico@users.noreply.github.com>
2024-01-15 13:00:44 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/promo-U-module-3-pair-13-sprint-2-hangman-game/" 
-})
+  base: '/hangman-game/',
+});
